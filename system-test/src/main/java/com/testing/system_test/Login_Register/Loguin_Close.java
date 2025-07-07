469627a9d6861de9a2174e0172235d45d44b5d00
kitze25 <sparta04@gmail.com>
2025-07-07 15:16:18 -0500

    Login/Reg completo

--- system-test/src/main/java/com/testing/system_test/Login_Register/Loguin_Close.java
+++ system-test/src/main/java/com/testing/system_test/Login_Register/Loguin_Close.java
@@ -1,6 +1,8 @@
 package com.testing.system_test.Login_Register;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -9,9 +11,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Login {
-    
-        public static void main(String[] args) {
+public class Loguin_Close {
+    public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\chromedriver-win64\\chromedriver.exe");
 
         ChromeOptions options = new ChromeOptions();
@@ -20,10 +21,20 @@ public class Login {
         options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
         options.setExperimentalOption("useAutomationExtension", false);
 
+        /* Desactiva guardado de constraseñas */
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        options.setExperimentalOption("prefs", prefs);
+
+        /* Evita detección de automatización */
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+        options.setExperimentalOption("useAutomationExtension", false);
+
         WebDriver driver = new ChromeDriver(options);
 
         try {
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
             driver.get("http://localhost:5173/");
 
             // 2. Espera y clic en "Inicia sesión aquí" para ir al registro
@@ -51,7 +62,12 @@ public class Login {
             System.out.println("Alerta: " + alert.getText());  // opcional
             alert.accept();
 
-            Thread.sleep(3000);
+            /* Escrito al final para ubicarlo al registrarse */
+            Thread.sleep(2000);
+            WebElement close_account = driver.findElement(By.xpath("//button[text()='Cerrar sesión']"));
+            close_account.click();
+
+            Thread.sleep(4000);
 
         } catch (Exception e) {
             e.printStackTrace();
