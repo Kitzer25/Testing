2125730676abc186c782af675e5aebe1a0178754
kitze25 <sparta04@gmail.com>
2025-07-07 12:37:46 -0500

    Inicio pruebas de sistema

---
+++ system-test/src/test/java/com/testing/system_test/SystemTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.system_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
